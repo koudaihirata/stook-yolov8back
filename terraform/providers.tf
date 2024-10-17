3a4642f6e7cefa1e5ddfa4525f38ef127a86b83c
ᎪᏩᎬᎻᎪ <hibikeita18@gmail.com>
2024-10-17 11:27:10 +0900

    fix: aws setup

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,13 +1,15 @@
 terraform {
-    required_providers {
+  required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.29.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.29.0"
     }
-    }
-    backend s3{
-        bucket = "stook-yolov8back-terraform"
-        region = "ap-northeast-3"
-        key = "prod.tfstate"
-    }
+  }
+  backend "s3" {
+    bucket = "stook-yolov8back-terraform"
+    region = "ap-northeast-3"
+    key    = "prod.tfstate"
+  }
 }
+
+data "aws_caller_identity" "current" {}
